9c9ac97b05802ae562fa97c7a3fef943c5565d0f
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-10-20 02:15:29 +0900

    merge branch into master

--- server/src/db/models/index.ts
+++ server/src/db/models/index.ts
@@ -1,14 +1,18 @@
 import { Sequelize } from 'sequelize';
 import User from './user';
 import Course from './course';
-
+// 設定をconfig.jsonからimport
 const { database, username, password, storage, dialect } = require('./config');
 
+// Sequelizeの初期化
 const sequelize = new Sequelize(database, username, password, {
+  // sqlを使うときはstorageが必要
   storage,
+  // どのデータベースを使うか
   dialect,
 });
 
+// テーブルの初期化
 const store = {
   User: User.initialize(sequelize),
   Course: Course.initialize(sequelize),
