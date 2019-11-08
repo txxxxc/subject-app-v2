b406dd3c496357a50cb90684a080ca2311a1fbd5
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-11-08 20:42:21 +0900

    install essential packages and settigs webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/App.tsx',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[id].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.js', '.ts', 'json'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+};
