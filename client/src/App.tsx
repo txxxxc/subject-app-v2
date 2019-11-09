dee2e4f9516cb4436d9b4f342cd26da893931980
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-11-09 15:46:49 +0900

    connect client to the apollo server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { createApolloClient } from '../utils/createApolloClient';
 
-ReactDOM.render(<div> Hello React!!</div>, document.getElementById('app'));
+const client = createApolloClient();
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <div>Hello React!!</div>
+    </ApolloHooksProvider>
+  </ApolloProvider>
+);
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ client/utils/createApolloClient.ts
@@ -0,0 +1,11 @@
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+export const createApolloClient = () =>
+  new ApolloClient({
+    link: createHttpLink({
+      uri: 'http://localhost:4000/graphql',
+    }),
+    cache: new InMemoryCache(),
+  });
