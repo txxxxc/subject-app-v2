4163f3f06331ff7558fa0c53fd21a3d24cea642f
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-12-24 21:50:43 +0900

    add-Card-Component

---
+++ client/src/components/molecules/Card/Card.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { muiTheme } from 'storybook-addon-material-ui';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import theme from '../../../utils/theme';
+import Card from './Card';
+
+const stories = storiesOf('Card', module);
+stories.addDecorator(muiTheme([theme]));
+stories.addParameters({ info: { inline: true } });
+stories.addDecorator(withKnobs);
+
+stories.add('カード', () => (
+  <Card
+    subjectName={text('科目', '数学Aα')}
+    blockName={text('ブロック', 'I_A')}
+  />
+));
