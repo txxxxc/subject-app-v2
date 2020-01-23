4a621c4d91dc2373b621cdb05105d5c8c8b63868
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-01-23 23:23:12 +0900

    fix Modal Component

--- client/src/components/atoms/Modal/Modal.tsx
+++ client/src/components/atoms/Modal/Modal.tsx
@@ -1,23 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import styled from 'styled-components';
 import { Modal as MuiModal } from '@material-ui/core';
 
 export interface ModalProps {
   open: boolean;
   handleClose?(): void;
+  children: ReactNode;
 }
 
 const Modal: FC<ModalProps> = (props: ModalProps) => {
   return (
     <ModalContainer open={props.open} onClose={props.handleClose}>
-      <Content></Content>
+      <Content>{props.children}</Content>
     </ModalContainer>
   );
 };
 
 const Content = styled.div`
   position: absolute;
-  height: 100%;
+  height: 70%;
+  width: 70%;
   background-color: white;
   top: 50%;
   left: 50%;
