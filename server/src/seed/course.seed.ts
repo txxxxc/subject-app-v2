bced4262ec5369a9948e73ca03f1ddc56d0d1251
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-02-03 14:27:52 +0900

    add seed data

--- server/src/seed/course.seed.ts
+++ server/src/seed/course.seed.ts
@@ -1,11 +1,400 @@
-/* eslint-disable @typescript-eslint/camelcase */
-export const courseSeed = [
-  {
-    id: '1',
-    course_name: 'math',
-    block: 'I_A',
-    teacher_name: 'hoge hoge',
-    room: '666',
-    is_compulsory: false,
-  },
-];
+export const courseSeed =[{
+  id: '0000',
+  course_name: '現代文',
+  block: 'I_A',
+  teacher_name: 'Aさん',
+  room: '000',
+  is_compulsory: true
+},
+{
+  id: '0001',
+  course_name: '古典',
+  block: 'I_B',
+  teacher_name: 'Bさん',
+  room: '001',
+  is_compulsory: false
+},
+{
+  id: '0002',
+  course_name: '数学',
+  block: 'II_A',
+  teacher_name: 'Cさん',
+  room: '002',
+  is_compulsory: false
+},
+{
+  id: '0003',
+  course_name: '生物',
+  block: 'II_B',
+  teacher_name: 'Dさん',
+  room: '003',
+  is_compulsory: true
+},
+{
+  id: '0004',
+  course_name: '科学',
+  block: 'III_A',
+  teacher_name: 'Eさん',
+  room: '004',
+  is_compulsory: false
+},
+{
+  id: '0005',
+  course_name: '物理',
+  block: 'III_B',
+  teacher_name: 'Fさん',
+  room: '005',
+  is_compulsory: false
+},
+{
+  id: '0006',
+  course_name: '地学',
+  block: 'IV_A',
+  teacher_name: 'Gさん',
+  room: '006',
+  is_compulsory: true
+},
+{
+  id: '0007',
+  course_name: '日本史',
+  block: 'IV_B',
+  teacher_name: 'Hさん',
+  room: '007',
+  is_compulsory: false
+},
+{
+  id: '0008',
+  course_name: '世界史',
+  block: 'V_A',
+  teacher_name: 'Iさん',
+  room: '008',
+  is_compulsory: false
+},
+{
+  id: '0009',
+  course_name: '英語',
+  block: 'V_B',
+  teacher_name: 'Aさん',
+  room: '009',
+  is_compulsory: true
+},
+{
+  id: '0010',
+  course_name: 'PE',
+  block: 'VI',
+  teacher_name: 'Bさん',
+  room: '010',
+  is_compulsory: false
+},
+{
+  id: '0011',
+  course_name: 'HFL',
+  block: 'I_A',
+  teacher_name: 'Cさん',
+  room: '011',
+  is_compulsory: false
+},
+{
+  id: '0012',
+  course_name: '現代文',
+  block: 'I_B',
+  teacher_name: 'Dさん',
+  room: '012',
+  is_compulsory: true
+},
+{
+  id: '0013',
+  course_name: '古典',
+  block: 'II_A',
+  teacher_name: 'Eさん',
+  room: '013',
+  is_compulsory: false
+},
+{
+  id: '0014',
+  course_name: '数学',
+  block: 'II_B',
+  teacher_name: 'Fさん',
+  room: '014',
+  is_compulsory: false
+},
+{
+  id: '0015',
+  course_name: '生物',
+  block: 'III_A',
+  teacher_name: 'Gさん',
+  room: '015',
+  is_compulsory: true
+},
+{
+  id: '0016',
+  course_name: '科学',
+  block: 'III_B',
+  teacher_name: 'Hさん',
+  room: '016',
+  is_compulsory: false
+},
+{
+  id: '0017',
+  course_name: '物理',
+  block: 'IV_A',
+  teacher_name: 'Iさん',
+  room: '017',
+  is_compulsory: false
+},
+{
+  id: '0018',
+  course_name: '地学',
+  block: 'IV_B',
+  teacher_name: 'Aさん',
+  room: '018',
+  is_compulsory: true
+},
+{
+  id: '0019',
+  course_name: '日本史',
+  block: 'V_A',
+  teacher_name: 'Bさん',
+  room: '019',
+  is_compulsory: false
+},
+{
+  id: '0020',
+  course_name: '世界史',
+  block: 'V_B',
+  teacher_name: 'Cさん',
+  room: '020',
+  is_compulsory: false
+},
+{
+  id: '0021',
+  course_name: '英語',
+  block: 'VI',
+  teacher_name: 'Dさん',
+  room: '021',
+  is_compulsory: true
+},
+{
+  id: '0022',
+  course_name: 'PE',
+  block: 'I_A',
+  teacher_name: 'Eさん',
+  room: '022',
+  is_compulsory: false
+},
+{
+  id: '0023',
+  course_name: 'HFL',
+  block: 'I_B',
+  teacher_name: 'Fさん',
+  room: '023',
+  is_compulsory: false
+},
+{
+  id: '0024',
+  course_name: '現代文',
+  block: 'II_A',
+  teacher_name: 'Gさん',
+  room: '024',
+  is_compulsory: true
+},
+{
+  id: '0025',
+  course_name: '古典',
+  block: 'II_B',
+  teacher_name: 'Hさん',
+  room: '025',
+  is_compulsory: false
+},
+{
+  id: '0026',
+  course_name: '数学',
+  block: 'III_A',
+  teacher_name: 'Iさん',
+  room: '026',
+  is_compulsory: false
+},
+{
+  id: '0027',
+  course_name: '生物',
+  block: 'III_B',
+  teacher_name: 'Aさん',
+  room: '027',
+  is_compulsory: true
+},
+{
+  id: '0028',
+  course_name: '科学',
+  block: 'IV_A',
+  teacher_name: 'Bさん',
+  room: '028',
+  is_compulsory: false
+},
+{
+  id: '0029',
+  course_name: '物理',
+  block: 'IV_B',
+  teacher_name: 'Cさん',
+  room: '029',
+  is_compulsory: false
+},
+{
+  id: '0030',
+  course_name: '地学',
+  block: 'V_A',
+  teacher_name: 'Dさん',
+  room: '030',
+  is_compulsory: true
+},
+{
+  id: '0031',
+  course_name: '日本史',
+  block: 'V_B',
+  teacher_name: 'Eさん',
+  room: '031',
+  is_compulsory: false
+},
+{
+  id: '0032',
+  course_name: '世界史',
+  block: 'VI',
+  teacher_name: 'Fさん',
+  room: '032',
+  is_compulsory: false
+},
+{
+  id: '0033',
+  course_name: '英語',
+  block: 'I_A',
+  teacher_name: 'Gさん',
+  room: '033',
+  is_compulsory: true
+},
+{
+  id: '0034',
+  course_name: 'PE',
+  block: 'I_B',
+  teacher_name: 'Hさん',
+  room: '034',
+  is_compulsory: false
+},
+{
+  id: '0035',
+  course_name: 'HFL',
+  block: 'II_A',
+  teacher_name: 'Iさん',
+  room: '035',
+  is_compulsory: false
+},
+{
+  id: '0036',
+  course_name: '現代文',
+  block: 'II_B',
+  teacher_name: 'Aさん',
+  room: '036',
+  is_compulsory: true
+},
+{
+  id: '0037',
+  course_name: '古典',
+  block: 'III_A',
+  teacher_name: 'Bさん',
+  room: '037',
+  is_compulsory: false
+},
+{
+  id: '0038',
+  course_name: '数学',
+  block: 'III_B',
+  teacher_name: 'Cさん',
+  room: '038',
+  is_compulsory: false
+},
+{
+  id: '0039',
+  course_name: '生物',
+  block: 'IV_A',
+  teacher_name: 'Dさん',
+  room: '039',
+  is_compulsory: true
+},
+{
+  id: '0040',
+  course_name: '科学',
+  block: 'IV_B',
+  teacher_name: 'Eさん',
+  room: '040',
+  is_compulsory: false
+},
+{
+  id: '0041',
+  course_name: '物理',
+  block: 'V_A',
+  teacher_name: 'Fさん',
+  room: '041',
+  is_compulsory: false
+},
+{
+  id: '0042',
+  course_name: '地学',
+  block: 'V_B',
+  teacher_name: 'Gさん',
+  room: '042',
+  is_compulsory: true
+},
+{
+  id: '0043',
+  course_name: '日本史',
+  block: 'VI',
+  teacher_name: 'Hさん',
+  room: '043',
+  is_compulsory: false
+},
+{
+  id: '0044',
+  course_name: '世界史',
+  block: 'I_A',
+  teacher_name: 'Iさん',
+  room: '044',
+  is_compulsory: false
+},
+{
+  id: '0045',
+  course_name: '英語',
+  block: 'I_B',
+  teacher_name: 'Aさん',
+  room: '045',
+  is_compulsory: true
+},
+{
+  id: '0046',
+  course_name: 'PE',
+  block: 'II_A',
+  teacher_name: 'Bさん',
+  room: '046',
+  is_compulsory: false
+},
+{
+  id: '0047',
+  course_name: 'HFL',
+  block: 'II_B',
+  teacher_name: 'Cさん',
+  room: '047',
+  is_compulsory: false
+},
+{
+  id: '0048',
+  course_name: '現代文',
+  block: 'III_A',
+  teacher_name: 'Dさん',
+  room: '048',
+  is_compulsory: true
+},
+{
+  id: '0049',
+  course_name: '古典',
+  block: 'III_B',
+  teacher_name: 'Eさん',
+  room: '049',
+  is_compulsory: false
+}]
