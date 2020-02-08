714b73be114fc0b53e945e21886a5cb0b91ef816
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-02-08 10:46:50 +0900

    fix TableBody

--- client/src/components/molecules/TableBody/TableBody.stories.tsx
+++ client/src/components/molecules/TableBody/TableBody.stories.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { muiTheme } from 'storybook-addon-material-ui';
-import { withKnobs } from '@storybook/addon-knobs';
-import theme from '../../../utils/theme';
-import TableBody, { TableBodyProps } from './TableBody';
+// import React from 'react';
+// import { storiesOf } from '@storybook/react';
+// import { muiTheme } from 'storybook-addon-material-ui';
+// import { withKnobs } from '@storybook/addon-knobs';
+// import theme from '../../../utils/theme';
+// import TableBody, { TableBodyProps } from './TableBody';
 
-const row: TableBodyProps = {
-  subjectRows: [
-    {
-      block: 'I_A',
-      courseName: '数学A',
-      teacherName: '田中太郎',
-      isCompulsory: true,
-    },
-  ],
-};
+// const row: TableBodyProps = {
+//   subjectRows: [
+//     {
+//       block: 'I_A',
+//       courseName: '数学A',
+//       teacherName: '田中太郎',
+//       isCompulsory: true,
+//     },
+//   ],
+// };
 
-const stories = storiesOf('TableBody', module);
-stories.addDecorator(muiTheme([theme]));
-stories.addParameters({ info: { inline: true } });
-stories.addDecorator(withKnobs);
+// const stories = storiesOf('TableBody', module);
+// stories.addDecorator(muiTheme([theme]));
+// stories.addParameters({ info: { inline: true } });
+// stories.addDecorator(withKnobs);
 
-stories.add('TableBody', () => <TableBody subjectRows={row.subjectRows} />);
+// stories.add('TableBody', () => <TableBody subjectRows={row.subjectRows} />);
