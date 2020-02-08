90afc19392730de69975070555b9bd9157b7cc9d
Tanaka-Tomoya <tomoya2627@gmail.com>
2020-02-08 10:48:03 +0900

    fix Table

--- client/src/components/organisms/Table/Table.tsx
+++ client/src/components/organisms/Table/Table.tsx
@@ -1,18 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { PageContext } from 'utils/contexts';
 import Table from '@material-ui/core/Table';
 import TableHead, { TableHeadProps } from '@/molecules/TableHead/TableHead';
-import TableBody, { TableBodyProps } from '@/molecules/TableBody/TableBody';
+import TableBody, { SubjectRow } from '@/molecules/TableBody/TableBody';
 
 export interface TableProps {
   head: TableHeadProps;
-  body: TableBodyProps;
+  body: SubjectRow[];
+  handleClose(): void;
 }
 
-const TableComponent: FC<TableProps> = (props: TableProps) => (
-  <Table>
-    <TableHead headElements={props.head.headElements} />
-    <TableBody subjectRows={props.body.subjectRows} />
-  </Table>
-);
+const TableComponent: FC<TableProps> = (props: TableProps) => {
+  const context = useContext(PageContext);
+  const onTableElementClick = (blockName, courseName) => {
+    localStorage[blockName] = courseName;
+    console.log(localStorage);
+    props.handleClose();
+    context.setClass({
+      ...context.class,
+      [blockName]: courseName,
+    });
+  };
+
+  return (
+    <Table>
+      <TableHead headElements={props.head.headElements} />
+      <TableBody
+        subjectRows={props.body}
+        onTableElementClick={onTableElementClick}
+      />
+    </Table>
+  );
+};
 
 export default TableComponent;
